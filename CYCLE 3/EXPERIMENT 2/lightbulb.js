4514845416364c21ed7f1951f303f1c8cac26e1a
minnajoby <24mp2277@rit.ac.in>
2024-11-17 23:19:58 +0530

    Add files via upload

---
+++ CYCLE 3/EXPERIMENT 2/lightbulb.js
@@ -0,0 +1,6 @@
+document.getElementById('onButton').addEventListener('click',function(){
+	document.getElementById('lightBulb').src='bulbon.jpg';
+});
+document.getElementById('offButton').addEventListener('click',function(){
+	document.getElementById('lightBulb').src='bulboff.jpg';
+});
