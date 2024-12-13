2d50e10972306d696954006649ab2141c2e4243b
minnajoby <24mp2277@rit.ac.in>
2024-12-13 13:14:29 +0530

    Add files via upload

---
+++ CYCLE 4/login.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include 'db_connection.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$email = $_POST['email'];
+	$pass = $_POST['password'];
+	$sql = "SELECT * FROM register WHERE email='$email' AND password='$pass'";
+	$result = $conn->query($sql);
+	if($result->num_rows > 0)
+	{
+		$row = $result->fetch_assoc();
+		$_SESSION['student_id'] = $row['id'];
+		header("Location:dashboard.php");
+		exit();
+	}
+	else
+	{
+		echo "User not found";
+	}
+	$conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+	<body>
+		<h2>Login</h2>
+		<form method="POST" action="login.php">
+			<label for="email">Email:</label><br>
+			<input type="email" id="email" name="email" required><br><br>
+			<label for="password">Password:</label><br>
+			<input type="password" id="password" name="password" required><br><br>
+			<input type="submit" value="Login">
+		</form>
+	</body>
+</html>
